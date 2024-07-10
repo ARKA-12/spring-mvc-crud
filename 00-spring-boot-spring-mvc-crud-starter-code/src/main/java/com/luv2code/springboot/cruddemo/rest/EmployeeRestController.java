37d0c185c07e57304ec2bd40cb0d9fd8dd53a7a8
Arka <arkamaity212@gmail.com>
2024-07-10 19:05:07 +0530

    adding 'employeeController.java' to show the list of employee. Add index.html to the satic folder to redirect the html page to list-employee.html page. Also add list-employee.html page where employee list can be seen and also use some bootstrap to look it better

--- 00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ 00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -1,87 +1,87 @@
-package com.luv2code.springboot.cruddemo.rest;
+// package com.luv2code.springboot.cruddemo.rest;
 
-import com.luv2code.springboot.cruddemo.entity.Employee;
-import com.luv2code.springboot.cruddemo.service.EmployeeService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+// import com.luv2code.springboot.cruddemo.entity.Employee;
+// import com.luv2code.springboot.cruddemo.service.EmployeeService;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+// import java.util.List;
 
-@RestController
-@RequestMapping("/api")
-public class EmployeeRestController {
+// @RestController
+// @RequestMapping("/api")
+// public class EmployeeRestController {
 
-    private EmployeeService employeeService;
+//     private EmployeeService employeeService;
 
-    @Autowired
-    public EmployeeRestController(EmployeeService theEmployeeService) {
-        employeeService = theEmployeeService;
-    }
+//     @Autowired
+//     public EmployeeRestController(EmployeeService theEmployeeService) {
+//         employeeService = theEmployeeService;
+//     }
 
-    // expose "/employees" and return a list of employees
-    @GetMapping("/employees")
-    public List<Employee> findAll() {
-        return employeeService.findAll();
-    }
+//     // expose "/employees" and return a list of employees
+//     @GetMapping("/employees")
+//     public List<Employee> findAll() {
+//         return employeeService.findAll();
+//     }
 
-    // add mapping for GET /employees/{employeeId}
+//     // add mapping for GET /employees/{employeeId}
 
-    @GetMapping("/employees/{employeeId}")
-    public Employee getEmployee(@PathVariable int employeeId) {
+//     @GetMapping("/employees/{employeeId}")
+//     public Employee getEmployee(@PathVariable int employeeId) {
 
-        Employee theEmployee = employeeService.findById(employeeId);
+//         Employee theEmployee = employeeService.findById(employeeId);
 
-        if (theEmployee == null) {
-            throw new RuntimeException("Employee id not found - " + employeeId);
-        }
+//         if (theEmployee == null) {
+//             throw new RuntimeException("Employee id not found - " + employeeId);
+//         }
 
-        return theEmployee;
-    }
+//         return theEmployee;
+//     }
 
-    // add mapping for POST /employees - add new employee
+//     // add mapping for POST /employees - add new employee
 
-    @PostMapping("/employees")
-    public Employee addEmployee(@RequestBody Employee theEmployee) {
+//     @PostMapping("/employees")
+//     public Employee addEmployee(@RequestBody Employee theEmployee) {
 
-        // also just in case they pass an id in JSON ... set id to 0
-        // this is to force a save of new item ... instead of update
+//         // also just in case they pass an id in JSON ... set id to 0
+//         // this is to force a save of new item ... instead of update
 
-        theEmployee.setId(0);
+//         theEmployee.setId(0);
 
-        Employee dbEmployee = employeeService.save(theEmployee);
+//         Employee dbEmployee = employeeService.save(theEmployee);
 
-        return dbEmployee;
-    }
+//         return dbEmployee;
+//     }
 
-    // add mapping for PUT /employees - update existing employee
+//     // add mapping for PUT /employees - update existing employee
 
-    @PutMapping("/employees")
-    public Employee updateEmployee(@RequestBody Employee theEmployee) {
+//     @PutMapping("/employees")
+//     public Employee updateEmployee(@RequestBody Employee theEmployee) {
 
-        Employee dbEmployee = employeeService.save(theEmployee);
+//         Employee dbEmployee = employeeService.save(theEmployee);
 
-        return dbEmployee;
-    }
+//         return dbEmployee;
+//     }
 
-    // add mapping for DELETE /employees/{employeeId} - delete employee
+//     // add mapping for DELETE /employees/{employeeId} - delete employee
 
-    @DeleteMapping("/employees/{employeeId}")
-    public String deleteEmployee(@PathVariable int employeeId) {
+//     @DeleteMapping("/employees/{employeeId}")
+//     public String deleteEmployee(@PathVariable int employeeId) {
 
-        Employee tempEmployee = employeeService.findById(employeeId);
+//         Employee tempEmployee = employeeService.findById(employeeId);
 
-        // throw exception if null
+//         // throw exception if null
 
-        if (tempEmployee == null) {
-            throw new RuntimeException("Employee id not found - " + employeeId);
-        }
+//         if (tempEmployee == null) {
+//             throw new RuntimeException("Employee id not found - " + employeeId);
+//         }
 
-        employeeService.deleteById(employeeId);
+//         employeeService.deleteById(employeeId);
 
-        return "Deleted employee id - " + employeeId;
-    }
+//         return "Deleted employee id - " + employeeId;
+//     }
 
-}
+// }
 
 
 
